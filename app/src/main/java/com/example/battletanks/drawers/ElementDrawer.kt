8ae59c0b2a7555e8ee11acaef82b261df5742dcc
205-22 <33nasrudin@gmail.com>
2025-03-22 12:20:17 +0300

    презентация 11 finish

--- app/src/main/java/com/example/battletanks/drawers/ElementDrawer.kt
+++ app/src/main/java/com/example/battletanks/drawers/ElementDrawer.kt
@@ -3,12 +3,9 @@ package com.example.battletanks.drawers
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
-import androidx.core.view.marginLeft
-import androidx.core.view.marginTop
+import androidx.annotation.DrawableRes
 import com.example.battletanks.CELL_SIZE
 import com.example.battletanks.R
-import com.example.battletanks.binding
-import com.example.battletanks.enums.Direction
 import com.example.battletanks.enums.Material
 import com.example.battletanks.models.Coordinate
 import com.example.battletanks.models.Element
@@ -33,7 +30,7 @@ class ElementsDrawer(val container: FrameLayout) {
         val viewOnCoordinate = getElementByCoordinates(coordinate,
             elementsOnContainer)
         if (viewOnCoordinate == null) {
-            drawView(coordinate)
+            selectMaterial(coordinate)
             return
         }
         if (viewOnCoordinate.material != currentMaterial) {
@@ -43,7 +40,7 @@ class ElementsDrawer(val container: FrameLayout) {
 
     private fun replaceView(coordinate: Coordinate){
         eraseView(coordinate)
-        drawView(coordinate)
+        selectMaterial(coordinate)
     }
 
     private fun eraseView(coordinate: Coordinate){
@@ -56,24 +53,43 @@ class ElementsDrawer(val container: FrameLayout) {
         }
     }
 
-    fun drawView(coordinate: Coordinate) {
+    fun selectMaterial(coordinate: Coordinate) {
         val view = ImageView(container.context)
         val layoutParams = FrameLayout.LayoutParams(CELL_SIZE, CELL_SIZE)
         when (currentMaterial) {
-            Material.EMPTY-> {
-
+            Material.EMPTY-> {}
+            Material.BRICK -> drawView(R.drawable.brick, coordinate)
+            Material.CONCRETE -> drawView(R.drawable.concrete, coordinate)
+            Material.GRASS -> drawView(R.drawable.grass, coordinate)
+            Material.EAGLE -> {
+                removeExistingEagle()
+                drawView(R.drawable.eagle, coordinate, 4, 3)
             }
-
-            Material.BRICK -> view.setImageResource(R.drawable.brick)
-            Material.CONCRETE -> view.setImageResource(R.drawable.concrete)
-            Material.GRASS-> view.setImageResource(R.drawable.grass)
         }
+
+    }
+
+    fun removeExistingEagle() {
+        elementsOnContainer.firstOrNull {  it.material == Material.EAGLE }?.coordinate?.let {
+            eraseView(it)
+        }
+    }
+
+    private fun drawView(
+        @DrawableRes image: Int,
+        coordinate: Coordinate,
+        width: Int = 1,
+        height: Int = 1
+    ) {
+        val view = ImageView(container.context)
+        val layoutParams = FrameLayout.LayoutParams(width * CELL_SIZE, height * CELL_SIZE)
+        view.setImageResource(image)
         layoutParams.topMargin = coordinate.top
         layoutParams.leftMargin = coordinate.left
         val viewId = View.generateViewId()
         view.id = viewId
         view.layoutParams = layoutParams
         container.addView(view)
-        elementsOnContainer.add(Element(viewId, currentMaterial, coordinate))
+        elementsOnContainer.add(Element(viewId, currentMaterial, coordinate, width, height))
     }
 }
--- app/src/main/java/com/example/battletanks/utils/Utils.kt
+++ app/src/main/java/com/example/battletanks/utils/Utils.kt
@@ -1,6 +1,7 @@
 package com.example.battletanks.utils
 
 import android.view.View
+import com.example.battletanks.CELL_SIZE
 import com.example.battletanks.binding
 import com.example.battletanks.models.Coordinate
 import com.example.battletanks.models.Element
@@ -11,6 +12,19 @@ fun View.checkViewCanMoveThroughBorder(coordinate: Coordinate) =
             coordinate.left >= 0 &&
             coordinate.left + this.width <= binding.container.width)
 
-fun getElementByCoordinates (coordinate: Coordinate,
-                             elementsOnContainer: List<Element>) =
-    elementsOnContainer.firstOrNull {it.coordinate == coordinate }
+fun getElementByCoordinates(coordinate: Coordinate, elementsOnContainer: List<Element>): Element? {
+    for (element in elementsOnContainer) {
+        for (height in 0 until element.height) {
+            for (width in 0 until element.width) {
+                val searchingCoordinate = Coordinate(
+                    element.coordinate.top + height * CELL_SIZE,
+                    element.coordinate.left + width * CELL_SIZE
+                )
+                if (coordinate == searchingCoordinate) {
+                    return element
+                }
+            }
+        }
+    }
+    return null
+}
